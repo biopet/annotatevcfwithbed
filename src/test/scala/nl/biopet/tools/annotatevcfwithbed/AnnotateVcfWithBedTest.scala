cfd40d4b4a838541459fa7aa8d2000814b592eb8
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:34 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/annotatevcfwithbed/AnnotateVcfWithBedTest.scala
+++ src/test/scala/nl/biopet/tools/annotatevcfwithbed/AnnotateVcfWithBedTest.scala
@@ -7,7 +7,7 @@ object AnnotateVcfWithBedTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      AnnotateVcfWithBed.main(Array())
     }
   }
 }
