154d80ed19768969feb89a45a6d25a5496df8f22
DavyCats <davycats.dc@gmail.com>
2017-10-13 13:17:44 +0200

    tool code

--- src/main/scala/nl/biopet/tools/annotatevcfwithbed/AnnotateVcfWithBed.scala
+++ src/main/scala/nl/biopet/tools/annotatevcfwithbed/AnnotateVcfWithBed.scala
@@ -1,6 +1,14 @@
 package nl.biopet.tools.annotatevcfwithbed
 
+import htsjdk.variant.variantcontext.VariantContextBuilder
+import htsjdk.variant.variantcontext.writer.{AsyncVariantContextWriter, VariantContextWriterBuilder}
+import htsjdk.variant.vcf.{VCFFileReader, VCFHeaderLineCount, VCFHeaderLineType, VCFInfoHeaderLine}
+
 import nl.biopet.utils.tool.ToolCommand
+import nl.biopet.utils.ngs.intervals.{BedRecord, BedRecordList}
+
+import scala.collection.JavaConversions._
+
 
 object AnnotateVcfWithBed extends ToolCommand {
   def main(args: Array[String]): Unit = {
@@ -10,8 +18,57 @@ object AnnotateVcfWithBed extends ToolCommand {
 
     logger.info("Start")
 
-    //TODO: Execute code
+    annotateVcfWithBed(cmdArgs)
 
     logger.info("Done")
+  }
+
+
+  def annotateVcfWithBed(cmdArgs: Args): Unit = {
+    val fieldType = cmdArgs.fieldType match {
+      case "Integer" => VCFHeaderLineType.Integer
+      case "Flag" => VCFHeaderLineType.Flag
+      case "Character" => VCFHeaderLineType.Character
+      case "Float" => VCFHeaderLineType.Float
+      case _ => VCFHeaderLineType.String
+    }
+
+    logger.info("Reading bed file")
+    val bedRecords = BedRecordList.fromFile(cmdArgs.bedFile).sorted
+
+    logger.info("Starting output file")
+
+    val reader = new VCFFileReader(cmdArgs.inputFile, false)
+    val header = reader.getFileHeader
+
+    val writer = new AsyncVariantContextWriter(
+      new VariantContextWriterBuilder()
+        .setOutputFile(cmdArgs.outputFile)
+        .setReferenceDictionary(header.getSequenceDictionary)
+        .build)
+
+    header.addMetaDataLine(
+      new VCFInfoHeaderLine(cmdArgs.fieldName,
+        VCFHeaderLineCount.UNBOUNDED,
+        fieldType,
+        cmdArgs.fieldDescription))
+    writer.writeHeader(header)
+
+    logger.info("Start reading vcf records")
+
+    for (record <- reader) {
+      val overlaps =
+        bedRecords.overlapWith(BedRecord(record.getContig, record.getStart, record.getEnd))
+      if (overlaps.isEmpty) {
+        writer.add(record)
+      } else {
+        val builder = new VariantContextBuilder(record)
+        if (fieldType == VCFHeaderLineType.Flag) builder.attribute(cmdArgs.fieldName, true)
+        else builder.attribute(cmdArgs.fieldName, overlaps.map(_.name).mkString(","))
+        writer.add(builder.make)
+      }
+    }
+    reader.close()
+    writer.close()
   }
 }
--- src/main/scala/nl/biopet/tools/annotatevcfwithbed/Args.scala
+++ src/main/scala/nl/biopet/tools/annotatevcfwithbed/Args.scala
@@ -2,4 +2,9 @@ package nl.biopet.tools.annotatevcfwithbed
 
 import java.io.File
 
-case class Args(inputFile: File = null)
+case class Args(inputFile: File = null,
+                bedFile: File = null,
+                outputFile: File = null,
+                fieldName: String = null,
+                fieldDescription: String = "",
+                fieldType: String = "String")
