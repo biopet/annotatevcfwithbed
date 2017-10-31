b771e3ae46ba20d9cf277c4e72140e665efde8c5
pjvan_thof <pjrvanthof@gmail.com>
2017-10-31 15:21:05 +0100

    Replace to new tool testing

--- src/main/scala/nl/biopet/tools/annotatevcfwithbed/AnnotateVcfWithBed.scala
+++ src/main/scala/nl/biopet/tools/annotatevcfwithbed/AnnotateVcfWithBed.scala
@@ -10,7 +10,7 @@ import nl.biopet.utils.ngs.intervals.{BedRecord, BedRecordList}
 import scala.collection.JavaConversions._
 
 
-object AnnotateVcfWithBed extends ToolCommand {
+object AnnotateVcfWithBed extends ToolCommand[Args] {
   def main(args: Array[String]): Unit = {
     val parser = new ArgsParser(toolName)
     val cmdArgs =
